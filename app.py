496049327fcb66b0d7e232bb27de1844cbe3d7f9
Jean-Marc <mjean_marc@hotmail.com>
2021-01-01 09:09:28 +0100

    Continue style home page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_fontawesome import FontAwesome
 
 # instantiate application
 app = Flask(__name__)
@@ -11,6 +11,8 @@ app.config['SECRET_KEY'] = "very-confidential-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///payprof.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+fa = FontAwesome(app)
 
 # create login manager
 login_manager = LoginManager()
